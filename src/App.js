440e7329a6febbb4b42cd2cb36fb937722d45fa1
finn-01 <nguyensam576@gmail.com>
2022-01-08 22:53:44 +0700

    usestate, loading

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import Tours from "./Tours";
 const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+	const [loading, setLoading] = useState(false);
+	const [tours, setTours] = useState([]);
+
+	const fetchTours = async () => {
+		setLoading(true);
+
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		);
+	}
+
 	return (
-		<div>
-			<h2>Tours Project Setup</h2>
-			<Loading />
+		<main>
 			<Tours />
-		</div>
+		</main>
 	);
 };
 
